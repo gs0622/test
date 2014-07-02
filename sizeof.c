fb973fcb083a451bfd762b0fff788970ac01d585
harrypan <gs0622@gmail.com>
2014-07-02 16:00:09 +0800

    time_t size

--- sizeof.c
+++ sizeof.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
-	unsigned short us;
-	unsigned int ui;
-	unsigned long ul;
-	unsigned long long ull;
-	/*check sizeof for various arch*/
-	us=sizeof(us);
-	ui=sizeof(ui);
-	ul=sizeof(ul);
-	ull=sizeof(ull);
-	printf("us=%d ui=%d ul=%d ull=%d\n", us, ui, ul, ull);
-	return 0;
+    unsigned short us;
+    unsigned int ui;
+    unsigned long ul;
+    unsigned long long ull;
+    time_t t;
+    /*check sizeof for various arch*/
+    us=sizeof(us);
+    ui=sizeof(ui);
+    ul=sizeof(ul);
+    ull=sizeof(ull);
+    t=sizeof(t);
+    printf("us=%d ui=%d ul=%d ull=%d t=%d\n", us, ui, ul, ull, t);
+    return 0;
 }
