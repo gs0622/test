fdd3b032f63432560c977d595263e6e00ba3f4e5
harrypan <gs0622@gmail.com>
2014-07-11 00:04:06 +0800

    code revise only, no functional change

--- strstr.c
+++ strstr.c
@@ -4,8 +4,11 @@ int main(int argc, char *argv[])
 {
     char *s;
     if (argc<3) return 0;
-    if (s = strstr(argv[1], argv[2])) /*argv[1]=string, argv[2]=pattern*/
-        printf("pattern \"%s\" is found in string \"%s\"\n", argv[2], argv[1]);
+    s = strstr(argv[1], argv[2]); /*argv[1]=string, argv[2]=pattern*/
+    if (s)
+        printf("pattern \"%s\" is found in string \"%s\" offset %ld\n", argv[2], argv[1], s-argv[1]);
     else 
         printf("pattern \"%s\" is not found in string \"%s\"\n", argv[2], argv[1]);
+    return 0;
 }
+
