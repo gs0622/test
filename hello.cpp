28047f8fc253c569fbe5d1bce0a18ddbaf2c1189
harrypan <gs0622@gmail.com>
2014-08-19 00:14:55 +0800

    update as OpenMP example

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <omp.h>
 
 using namespace std;
 
 int main(void)
 {
-    int a, b;
-    cin >> a >> b;
-    cout << "hello world " << a << " " << b;
+    int th_id, nthreads;
+    #pragma omp parallel private(th_id) shared(nthreads)
+    {
+        th_id = omp_get_thread_num();
+        #pragma omp critical
+        {
+            cout << "hello world " << th_id << endl;
+        }
+        #pragma omp barrier
+
+        #pragma omp master
+        {
+            nthreads = omp_get_num_threads();
+            cout << "There are " << nthreads << " threads" << '\n';
+        }
+    }
     return 0;
 }
