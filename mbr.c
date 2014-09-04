bdc564021d1889520982ae1c6addc6562bbaa0dd
harrypan <gs0622@gmail.com>
2014-09-04 09:46:48 +0800

    MBR read draft

---
+++ mbr.c
@@ -0,0 +1,28 @@
+/*mbr read example*/
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef struct {
+    char code[440];
+    char ignore[6];
+    char partation[4][16];
+    char signature[2];
+} mbr_t;
+
+int main(int argc, char *argv[])
+{
+    int fd, nread;
+    mbr_t mbr;
+    if (1 == argc) return 0;
+    printf("fi=%s\n", argv[1]);
+    fd = open(argv[1], 'r');
+    printf("fd=%d\n", fd);
+    nread = read(fd, &mbr, sizeof(mbr));
+    printf("nread=%d\n", nread);
+    if (fd > 0) close(fd);
+    return 0;
+}
+
