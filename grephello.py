4a70b1c551ec75e145835ff05177a6cb141a7ec7
Harry Pan <harry.pan@intel.com>
2016-03-23 19:40:09 +0800

    Grep 'hello' string via remote ssh
    This script utilizes sshpass, ssh to issue grep command remotely
    so as to preform stress test to see whether device generates crash,
    thus it would consolidate patch validation.

---
+++ grephello.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import time
+import datetime
+import subprocess
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description='grep "hello" string remotely')
+	parser.add_argument('--remote', dest='remote', type=str, default='10.5.232.37', help='rermote IP address')
+	parser.add_argument('--loops', dest='loops', type=int, default=11, help='iterator loops')
+	args = parser.parse_args()
+	print args
+
+	prefix = ['sshpass', '-p', 'test0000', 'ssh',
+		'-oStrictHostKeyChecking=no', '-oConnectTimeout=10',
+		'-oServerAliveInterval=5' ]
+	cmd1 = ['grep -rsI "hello" / --exclude={kmsg,pagemap} --exclude-dir=sys &> /dev/null']
+	cmd2 = ['grep -rsI "hello" /sys --exclude-dir={usb,tracing,ehdaudio0D2} &> /dev/null']
+	cmd3 = ['uptime']
+
+	prefix.append("root@" + args.remote)
+
+	for i in range(args.loops):
+		now = str(datetime.datetime.fromtimestamp(time.time()))
+		print now, ": ", str(i).rjust(4), ": cmd1: ", cmd1
+		subprocess.call(prefix + cmd1)
+
+		now = str(datetime.datetime.fromtimestamp(time.time()))
+		print now, ": ", str(i).rjust(4), ": cmd2: ", cmd2
+		subprocess.call(prefix + cmd2)
+
+		now = str(datetime.datetime.fromtimestamp(time.time()))
+		print now, ": ", str(i).rjust(4), ": cmd3: ", cmd3
+		subprocess.call(prefix + cmd3)
+
+if __name__ == '__main__':
+	main()
