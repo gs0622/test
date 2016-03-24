7012dcd34e5300edc8d2ca8867cf20ac84d92efc
Harry Pan <harry.pan@intel.com>
2016-03-24 17:15:55 +0800

    Revise script to fit various platform
    This patch refines script using argparser, tinkers test sequence,
    log formation, ssh error handling ...etc.
    
    It is equivalent to
    echo 3 > /proc/sys/kernel/hung_task_timeout_secs
    echo HUNG_TASK > /sys/kernel/debug/provoke-crash/DIRECT &

--- hungtask.py
+++ hungtask.py
@@ -1,54 +1,61 @@
 #!/usr/bin/python
-import sys
 import time
 import datetime
 import subprocess
+import argparse
 
 def main():
-	loops = 10
-	remote = "10.174.88.78"
-	prefix = ['sshpass', '-p', 'test0000', 'ssh',
-		'-oStrictHostKeyChecking=no', '-oConnectTimeout=5' ]
-	cmd1 = ['echo 5 > /proc/sys/kernel/hung_task_timeout_secs']
+	parser = argparse.ArgumentParser(description='perform "HUNG_TASK" test via remote ssh')
+	parser.add_argument('--remote', dest='remote', type=str, metavar='IP', default='10.5.232.37', help='rermote IP address')
+	parser.add_argument('--loops', dest='loops', type=int, default=10, help='iterator loops')
+	parser.add_argument('--sleep', dest='sleep', type=int, default=30, help='sleep seconds per test')
+	args = parser.parse_args()
+	print args
+
+	prefix = ['sshpass', '-p', 'test0000', 'ssh', '-q',
+		'-oStrictHostKeyChecking=no', '-oUserKnownHostsFile=/dev/null',
+		'-oConnectTimeout=10', '-oServerAliveInterval=5' ]
+	cmd1 = ['echo 3 > /proc/sys/kernel/hung_task_timeout_secs']
 	cmd2 = ['cat /proc/sys/kernel/hung_task_timeout_secs']
 	cmd3 = ['echo HUNG_TASK > /sys/kernel/debug/provoke-crash/DIRECT &']
 	cmd4 = ['dmesg | tail -3']
+	cmd5 = ['i=1; while true; do echo $i; let i++; sleep 1; done']
 
-	if len(sys.argv) > 1:
-		remote = sys.argv[1]
+	prefix.append("root@" + args.remote)
 
-	if len(sys.argv) > 2:
-		loops = int(sys.argv[2])
-
-	prefix.append("root@" + remote)
-
-	for i in range(loops):
+	for i in range(args.loops):
 		try:
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now, " cmd1: ", cmd1
+			print now, ": ", str(i).rjust(4), ": cmd1: ", cmd1
 			ret = subprocess.check_output(prefix + cmd1)
 
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now, " cmd2: ", cmd2
+			print now, ": ", str(i).rjust(4), ": cmd2: ", cmd2
 			ret = subprocess.check_output(prefix + cmd2)
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now + " hung_task_timeout_secs=" + ret
+			print now, ": ", str(i).rjust(4), ": hung_task_timeout_secs:", ret
 
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now, " cmd3: ", cmd3
+			print now, ": ", str(i).rjust(4), ": cmd3: ", cmd3
 			subprocess.call(prefix + cmd3)
 
-
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now, " cmd4: ", cmd4
+			print now, ": ", str(i).rjust(4), ": cmd4: ", cmd4
 			ret = subprocess.check_output(prefix + cmd4)
 			now = str(datetime.datetime.fromtimestamp(time.time()))
-			print now, " dmesg=\n", ret
+			print now, ": ", str(i).rjust(4), ": dmesg:\n", ret
 
-			time.sleep(10)
+			now = str(datetime.datetime.fromtimestamp(time.time()))
+			print now, ": ", str(i).rjust(4), ": cmd5: ", cmd5
+			subprocess.call(prefix + cmd5)
+
+			now = str(datetime.datetime.fromtimestamp(time.time()))
+			print now, ": ", str(i).rjust(4), ": sleep: ", args.sleep
+			time.sleep(args.sleep)
 
 		except subprocess.CalledProcessError as err:
-			print "error code: ", err.returncode
+			now = str(datetime.datetime.fromtimestamp(time.time()))
+			print now, ": ", str(i).rjust(4), ": error code: ", err.returncode
 			break;
 
 if __name__ == '__main__':
