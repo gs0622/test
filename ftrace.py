b83b7dc3b6920ee5c0ad63e1a2966401272598d5
Harry Pan <harry.pan@intel.com>
2016-04-14 23:40:15 +0800

    Revise to add ftrace test item
    This patch naively adds test of function_graph tracer then fall
    to function tracer

--- ftrace.py
+++ ftrace.py
@@ -11,13 +11,28 @@ def main():
 	print args
 
 	prefix = ['sshpass', '-p', 'test0000', 'ssh',
-		'-oStrictHostKeyChecking=no', '-oConnectTimeout=10',
-		'-oServerAliveInterval=5' ]
+		'-oStrictHostKeyChecking=no', '-oConnectTimeout=3',
+		'-oServerAliveInterval=3' ]
+	cmd0 = ['echo function_graph > /sys/kernel/debug/tracing/current_tracer']
 	cmd1 = ['echo function > /sys/kernel/debug/tracing/current_tracer']
 	cmd2 = ['echo 1 > /sys/kernel/debug/tracing/tracing_on']
 	cmd3 = ['cat /sys/kernel/debug/tracing/trace']
 
 	prefix.append("root@" + args.remote)
+
+	now = str(datetime.datetime.fromtimestamp(time.time()))
+	print now, ": ", ": cmd0: ", cmd0
+	subprocess.call(prefix + cmd0)
+
+	now = str(datetime.datetime.fromtimestamp(time.time()))
+	print now, ": ", ": cmd2: ", cmd2
+	subprocess.call(prefix + cmd2)
+
+	now = str(datetime.datetime.fromtimestamp(time.time()))
+	print now, ": ", ": cmd3: ", cmd3
+	subprocess.call(prefix + cmd3)
+
+	time.sleep(2)
 
 	now = str(datetime.datetime.fromtimestamp(time.time()))
 	print now, ": ", ": cmd1: ", cmd1
